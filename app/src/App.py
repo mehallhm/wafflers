8ac32b47205c2bcab8365b53d429e50f13bb5574
Juwang110 <Juwang110@gmail.com>
2024-05-30 21:56:30 +0200

    added landing page template with button navigation

--- app/src/App.py
+++ app/src/App.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
 def App():
-    st.write("Hello")
-    'World'
+    st.title('Welcome to Carbon Connect!')
+    st.header('Choose your persona below')
+
+    if st.button("General User"):
+      st.switch_page("pages/01_userhome.py")
+    if st.button("Enterprise"):
+      st.switch_page("pages/02_enterprisehome.py")
+    if st.button("NGO"):
+      st.switch_page("pages/03_NGOhome.py")
+      
 
 App()
---
+++ app/src/pages/02_enterprisehome.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def App():
+    st.write("Enterprise home page")
+
+    
+App()
