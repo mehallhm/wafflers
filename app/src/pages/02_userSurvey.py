da9d0faab356f2bcd88750f9f2df76ea97e5fb72
Babalola-a <147947002+Babalola-a@users.noreply.github.com>
2024-06-05 17:44:20 +0200

    adding format of survey

--- app/src/pages/02_userSurvey.py
+++ app/src/pages/02_userSurvey.py
@@ -1,7 +1,17 @@
 import streamlit as st
 from modules.nav import SideBarLinks
+import streamlit_survey as ss
+survey = ss.StreamlitSurvey()
 
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
 st.write("# User survey")
+
+survey.text_input("Text input:", id="Q1")
+st.write("## What country in Europe are you from?")
+survey.selectbox("Selection box:", options=["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan"])
+
+
+carNumber = st.slider("How many cars do you currently have?", 0, 130, 25)
+st.write("I have ", carNumber , "cars")
