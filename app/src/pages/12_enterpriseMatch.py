c2e0c649f7e3f051e435cbad9137d62d7328c100
Juwang110 <Juwang110@gmail.com>
2024-06-08 10:54:11 +0200

    Updated display visuals for match tools, updated comparison tool display

--- app/src/pages/12_enterpriseMatch.py
+++ app/src/pages/12_enterpriseMatch.py
@@ -30,11 +30,36 @@ except:
   st.write("**Important**: Could not connect to sample api, so using dummy data.")
   data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
 
-st.dataframe(data)
+transport = []
+flights = []
+energy = []
+heat = []
 
-st.write('## Comparison to average company in your country')
+for item in data:
+    if item['description'] == 'Transport':
+        transport.append(item['name'])
+    elif item['description'] == 'Flights':
+        flights.append(item['name'])
+    elif item['description'] == 'Energy':
+        energy.append(item['name'])
+    elif item['description'] == 'Heat':
+        heat.append(item['name'])
 
-st.write('')
+with st.expander("Transport NGOs", expanded=False):
+    st.write("#### NGO list:")
+    st.markdown("- " + "\n- ".join(transport))
+
+with st.expander("Flights NGOs", expanded=False):
+    st.write("#### NGO list:")
+    st.markdown("- " + "\n- ".join(flights))
+
+with st.expander("Energy NGOs", expanded=False):
+    st.write("#### NGO list:")
+    st.markdown("- " + "\n- ".join(energy))
+
+with st.expander("Heat NGOs", expanded=False):
+    st.write("#### NGO list:")
+    st.markdown("- " + "\n- ".join(heat))
 
 data = {} 
 try:
@@ -43,4 +68,14 @@ except:
   st.write("**Important**: Could not connect to sample api, so using dummy data.")
   data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
 
-st.dataframe(data)
+avg_emission = data[0]["AVG Emission (by Country in kilotonnes)"]
+country = data[0]["Country"]
+your_emissions = data[0]["Your Emissions (in kilotonnes)"]
+multiplier = your_emissions / avg_emission
+
+st.write('')
+
+if st.button("View comparison"):
+   st.write("#### In", country + ", your emissions in kilotonnes is ", your_emissions, ", while the average enterprise's is ", avg_emission)
+   st.write("#### That means your emissions are", multiplier, "times the average enterprise's emissions in " + country)
+
--- app/src/pages/22_NGOMatch.py
+++ app/src/pages/22_NGOMatch.py
@@ -31,7 +31,36 @@ except:
   st.write("**Important**: Could not connect to sample api, so using dummy data.")
   data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
 
-st.dataframe(data)
+transport = []
+flights = []
+energy = []
+heat = []
+
+for item in data:
+    if item['description'] == 'Transport':
+        transport.append(item['name'])
+    elif item['description'] == 'Flights':
+        flights.append(item['name'])
+    elif item['description'] == 'Energy':
+        energy.append(item['name'])
+    elif item['description'] == 'Heat':
+        heat.append(item['name'])
+
+with st.expander("Transport NGOs", expanded=False):
+    st.write("#### NGO list:")
+    st.markdown("- " + "\n- ".join(transport))
+
+with st.expander("Flights NGOs", expanded=False):
+    st.write("#### NGO list:")
+    st.markdown("- " + "\n- ".join(flights))
+
+with st.expander("Energy NGOs", expanded=False):
+    st.write("#### NGO list:")
+    st.markdown("- " + "\n- ".join(energy))
+
+with st.expander("Heat NGOs", expanded=False):
+    st.write("#### NGO list:")
+    st.markdown("- " + "\n- ".join(heat))
 
 st.write("## Users you matched with")
 
